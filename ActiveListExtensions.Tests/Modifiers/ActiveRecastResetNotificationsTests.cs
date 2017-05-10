7c49738b6ba99cb148507525a98c2bb893f32f54
Johannes Moersch <Johannes.Moersch@gmail.com>
2017-05-10 00:05:48 -0600

    Adds additional unit test.

--- ActiveListExtensions.Tests/Modifiers/ActiveRecastResetNotificationsTests.cs
+++ ActiveListExtensions.Tests/Modifiers/ActiveRecastResetNotificationsTests.cs
@@ -1,6 +1,8 @@
 ﻿using ActiveListExtensions.Tests.Helpers;
+using ActiveListExtensions.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -153,7 +155,7 @@ namespace ActiveListExtensions.Tests.Modifiers
 		}
 
 		[Fact]
-		public void RandomOperations()
+		public void RandomReplaceOperations()
 		{
 			RandomGenerator.ResetRandomGenerator();
 
@@ -168,6 +170,62 @@ namespace ActiveListExtensions.Tests.Modifiers
 				value.Value = RandomGenerator.GenerateRandomIntegerList(count, 0, 50);
 
 				Assert.True(value.Value.SequenceEqual(sut));
+			}
+		}
+
+		[Fact]
+		public void RandomChangeOperations()
+		{
+			RandomGenerator.ResetRandomGenerator();
+
+			var value = new ActiveValue<IEnumerable<object>>(Enumerable.Range(0, 50).Select(i => new object()));
+
+			var sut = value.ToActiveList().ActiveRecastResetNotifications();
+
+			int addCount = 0;
+			int removeCount = 0;
+			sut.CollectionChanged += (s, e) =>
+			{
+				if (e.Action == NotifyCollectionChangedAction.Add)
+					++addCount;
+				if (e.Action == NotifyCollectionChangedAction.Remove)
+					++removeCount;
+			};
+
+			for (int i = 0; i < 100; ++i)
+			{
+				var list = new ObservableList<object>();
+				foreach (var item in sut)
+					list.Add(list.Count, item);
+
+				int targetAddCount = 0;
+				int targetRemoveCount = 0;
+				foreach (var index in Enumerable.Range(0, 10))
+				{
+					if (list.Count > 0)
+					{
+						++targetRemoveCount;
+						list.Remove(RandomGenerator.GenerateRandomInteger(0, list.Count));
+					}
+					break;
+				}
+				foreach (var index in Enumerable.Range(0, 15))
+				{
+					++targetAddCount;
+					list.Add(RandomGenerator.GenerateRandomInteger(0, list.Count), new object());
+					break;
+				}
+				foreach (var index in Enumerable.Range(0, 10))
+					list.Move(RandomGenerator.GenerateRandomInteger(0, list.Count), RandomGenerator.GenerateRandomInteger(0, list.Count));
+
+				value.Value = list;
+
+				Assert.Equal(addCount, targetAddCount);
+				Assert.Equal(removeCount, targetRemoveCount);
+				Assert.True(value.Value.SequenceEqual(sut));
+
+				addCount = 0;
+				removeCount = 0;
 			}
 		}
 	}
