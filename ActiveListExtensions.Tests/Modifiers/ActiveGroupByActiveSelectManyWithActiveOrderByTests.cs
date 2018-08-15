d35f71cccd8d63987bfe7c317c2388fdd332b0f6
Johannes Moersch <Johannes.Moersch@gmail.com>
2018-08-15 15:15:53 -0600

    Adds a few unit tests for a combination of extensions.

---
+++ ActiveListExtensions.Tests/Modifiers/ActiveGroupByActiveSelectManyWithActiveOrderByTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActiveListExtensions.Tests.Helpers;
+using Xunit;
+
+namespace ActiveListExtensions.Tests.Modifiers
+{
+	public class ActiveGroupByActiveSelectManyWithActiveOrderByTests
+	{
+		[Fact]
+		public void RandomlyInsertItems() => CollectionTestHelpers.RandomlyInsertItems(ActiveGroupByThenActiveSelectManyWithActiveOrderBy, GroupByThenSelectManyWithOrderBy, () => (RandomGenerator.GenerateRandomInteger(), RandomGenerator.GenerateRandomInteger()));
+
+		[Fact]
+		public void RandomlyRemoveItems() => CollectionTestHelpers.RandomlyRemoveItems(ActiveGroupByThenActiveSelectManyWithActiveOrderBy, GroupByThenSelectManyWithOrderBy, () => (RandomGenerator.GenerateRandomInteger(), RandomGenerator.GenerateRandomInteger()));
+
+		[Fact]
+		public void RandomlyReplaceItems() => CollectionTestHelpers.RandomlyReplaceItems(ActiveGroupByThenActiveSelectManyWithActiveOrderBy, GroupByThenSelectManyWithOrderBy, () => (RandomGenerator.GenerateRandomInteger(), RandomGenerator.GenerateRandomInteger()));
+
+		[Fact]
+		public void RandomlyMoveItems() => CollectionTestHelpers.RandomlyMoveItems(ActiveGroupByThenActiveSelectManyWithActiveOrderBy, GroupByThenSelectManyWithOrderBy, () => (RandomGenerator.GenerateRandomInteger(), RandomGenerator.GenerateRandomInteger()));
+
+		[Fact]
+		public void ResetWithRandomItems() => CollectionTestHelpers.ResetWithRandomItems(ActiveGroupByThenActiveSelectManyWithActiveOrderBy, GroupByThenSelectManyWithOrderBy, () => (RandomGenerator.GenerateRandomInteger(), RandomGenerator.GenerateRandomInteger()));
+
+		private IActiveList<(int key, int value)> ActiveGroupByThenActiveSelectManyWithActiveOrderBy(IActiveList<(int key, int value)> list)
+			=> list
+				.ActiveGroupBy(item => item.key)
+				.ActiveSelectMany(group => group.ActiveOrderBy(item => item.value, ListSortDirection.Ascending));
+
+		private IEnumerable<(int key, int value)> GroupByThenSelectManyWithOrderBy(IReadOnlyList<(int key, int value)> list)
+			=> list
+				.GroupBy(item => item.key)
+				.SelectMany(group => group.OrderBy(item => item.value));
+	}
+}
